d22d46da078fd6939f3d0a1e8cc87e1ae47f3477
AS2POKER <projets.colab@gmail.com>
2025-08-12 16:11:07 -0400

    Revert "j'ai rajoute des modif derniere minutes"
    This reverts commit 71966c046375dc3cbd92c1af7d6eed113d05e59d.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -11,5 +11,5 @@ shipping_cost = weight * rate
 print(f"Shipping Cost: {shipping_cost} USD")
 
 # Voici une nouvelle mise à jour par <your GitHub username>
-# Here is another update by <your GitHub AS2POKER>
+
 
